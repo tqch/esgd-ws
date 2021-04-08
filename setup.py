71dc18c113bdf286556ed6863ffe3dc339aecdfd
tqch <tqch2020@gmail.com>
2021-04-08 17:07:58 -0400

    pre release v3

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+
+def load_requirements():
+    with open("requirements.txt", "r") as f:
+        return f.readlines()
+
 
 setup(
     name="esgd-ws",
-    version="0.0.9",
+    version="0.1.0",
     author="Tianqi Chen",
     description="Evolutionary Stochastic Gradient Descent with Weight Sampling",
     license="MIT",
     url="https://github.com/tqch/esgd-ws",
-    packages=["models"],
-    py_modules=["esgd","esgd-ws"]
+    package=find_packages(),
+    install_requires=load_requirements()
 )
